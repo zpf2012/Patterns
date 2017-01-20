57fb442e896b069bd2a8482c1d3c18a8de0cb030
pengfei.zhang@hand-china.com <pengfei.zhang@hand-china.com>
2017-01-20 14:26:22 +0800

    装饰器模式

--- DecoratorPattern/src/com/hand/decorator_pattern/service/impl/Main.java
+++ DecoratorPattern/src/com/hand/decorator_pattern/service/impl/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		Shape circle = new Circle();
 		
-		Shape redCircle = new RedShapeDecorator(new Rectangle());
+		Shape redCircle = new RedShapeDecorator(new Circle());
 		
-		Shape redRectangle = new RedShapeDecorator(new Circle());
+		Shape redRectangle = new RedShapeDecorator(new Rectangle());
 		
 		System.out.println("Circle with normal border.");
 		circle.draw();
