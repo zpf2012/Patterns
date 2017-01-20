f5a14160813fedc1a25c6b95ea3ceb641934971b
pengfei.zhang@hand-china.com <pengfei.zhang@hand-china.com>
2017-01-20 11:28:16 +0800

    组合模式

---
+++ CompositePattern/src/com/hand/composite_pattern/dto/Employee.java
@@ -0,0 +1,36 @@
+package com.hand.composite_pattern.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+
+	private String name;
+	private String dept;
+	private int salary;
+	private List<Employee> subordinates;
+	public Employee(String name, String dept, int salary) {
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+		subordinates = new ArrayList<Employee>();
+	}
+	
+	public void add(Employee employee){
+		subordinates.add(employee);
+	}
+	
+	public void remove(Employee employee){
+		subordinates.remove(employee);
+	}
+	
+	public List<Employee> getSubordinates() {
+		return subordinates;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+	
+}
